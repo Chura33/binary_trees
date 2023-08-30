7afd7f001fc99323eeab58121684bc211136201d
Chura33 <churabrady@gmail.com>
2023-08-30 06:25:28 -0700

    solved task 1 binary left

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -10,24 +10,22 @@
 
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
-	binary_tree_t *node;
+	binary_tree_t *new_node;
 
 	if (parent == NULL)
 		return (NULL);
-	node = binary_tree_node(parent,value);
 
-	if (!node)
+	new_node = binary_tree_node(parent, value);
+	if (new_node == NULL)
 		return (NULL);
 
-	if (parent->left == NULL)
+	if (parent->left != NULL)
 	{
-		parent->left = node;
+		new_node->left = parent->left;
+		parent->left->parent = new_node;
 	}
-	else
-	{
-		node->left = parent->left;
-		parent->left->parent = node;
+	parent->left = new_node;
+
+	return (new_node);
 	}
-	return (node);
-}
 
