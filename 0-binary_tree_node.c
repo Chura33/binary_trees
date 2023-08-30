f9526e233bf24ae4b9065e926f25c5d3781ee4ba
Chura33 <churabrady@gmail.com>
2023-08-30 04:46:55 -0700

    solved 0-binary_tree_node.c

---
+++ 0-binary_tree_node.c
@@ -0,0 +1,17 @@
+#include "binary_trees.h"
+#include <stdlib.h>
+/**
+* binary_tree_node - returns a pointer to a binary tree node
+* @parent: pointer to the parent node
+* @value: the value in the node
+*/
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
+{
+	binary_tree_t *node = malloc(sizeof(binary_tree_t));
+
+	node->n = value;
+	node->parent = parent;
+	node->left = NULL;
+	node->right = NULL;
+	return (node);
+}
